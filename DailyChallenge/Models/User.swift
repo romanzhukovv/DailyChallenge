f86055cb14e6731f686b491fcb45c4d673d255d8
romanzhukovv <zhukov.roman89@gmail.com>
2022-12-19 23:46:46 +0400

    added User model

---
+++ DailyChallenge/Models/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  DailyChallenge
+//
+//  Created by Roman Zhukov on 19.12.2022.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let favoriteCategories: [Challenge.Category]
+}
